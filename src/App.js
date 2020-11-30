bf9607c1e119021b4fe05ed3ae74e27113e31055
ToveXan <zxanoswork@gmail.com>
2020-11-30 17:43:42 -0600

    practicing css

--- src/App.js
+++ src/App.js
@@ -8,9 +8,29 @@ class App extends React.Component {
   render(){
     return (
       <div className="test-div">
-        <header>
-          <p>header</p>
-        </header>
+        <nav className="navbar">
+          <div className="max-width">
+            <div className="logo">
+              <ul className="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <section class="home" id="home">
+          <div class="max-width">
+            <div className="home-content">
+              <div className="text-1">Hello, my name is</div>
+              <div className="text-2">Zoe Xanos</div>
+            </div>
+          </div>
+        </section>
         <h1>main body</h1>
         {/* <img src={mainimg} alt="Logo" height="50%" width="50%"/> */}
         <GridController />
--- src/Controllers/GridController.js
+++ src/Controllers/GridController.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../scss/GridController.scss';
 
 class GridController extends React.Component {
   render(){
     return(
         <div>
-          <h3>gridcontroller</h3>
+          <div className="pal-1"></div>
+          <div className="pal-2"></div>
+          <div className="pal-3"></div>
+          <div className="pal-4"></div>
+          <div className="pal-5"></div>
         </div>
     )
   }
