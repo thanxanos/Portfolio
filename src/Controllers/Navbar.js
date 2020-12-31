0ea90c3529a743c5d17a8a563fa2ab5688506c15
ToveXan <zxanoswork@gmail.com>
2020-12-30 22:09:55 -0600

    navbar toggle finished!

--- src/Controllers/Navbar.js
+++ src/Controllers/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../scss/Navbar.scss';
+import * as FaIcons from 'react-icons/fa';
 
 
-class Navbar extends React.Component {
-  render(){
+function Navbar(){
+  const [navbar, setNavbar] = useState(false);
+  const showNavbar = () => setNavbar(!navbar);
+
     return (
         <nav className="navbar">
           <div className="max-width">
             <div className="logo"><a href="#">Zoe Xanos</a></div>
-            <ul className="menu">
+            <ul className={ navbar ? 'menu active': 'menu'}>
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#skills">Skills</a></li>
@@ -16,10 +19,13 @@ class Navbar extends React.Component {
               <li><a href="#blog">Blog</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
+            <div className="menu-btn" onClick={showNavbar}>
+             { navbar ? <FaIcons.FaTimes className='side-btn'/> : <FaIcons.FaBars className='side-btn'/>}
+            </div>
           </div>
         </nav>
     );
-  }
+  
 }
 
 export default Navbar;
