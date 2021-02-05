111dcff2fa00577541c117ff65b4de10c21947ea
ToveXan <zxanoswork@gmail.com>
2021-02-05 15:55:05 -0600

    contact and footer are done

--- src/Controllers/FooterController.js
+++ src/Controllers/FooterController.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import '../scss/Footer.scss';
+import twitter from './../footerIcons/twitter.png';
+import github from './../footerIcons/github.png';
+import linkedin from './../footerIcons/linkedin.png';
+import medium from './../footerIcons/medium.png';
 
 class FooterController extends React.Component {
   render(){
     return(
-      <div id='footer'>
-        <p>Footer insert icons here</p>
+      <div className='footer'>
+        <div className='footer-wrapper'>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/zoexanos"><img src={twitter} alt="twitlogo" /></a>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/zoexanos"><img src={github} alt="gitlogo" /></a>
+          <a
+          className="mediumlogo"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://zoexanos.medium.com/"><img src={medium} alt="mediumlogo" /></a>
+          <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/zoexanos/"><img src={linkedin} alt="linkedinlogo" /></a>
+        </div>
       </div>
     )
   }
