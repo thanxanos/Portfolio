754665d89e38de90274e41b8af6231cf9ab446ca
ToveXan <zxanoswork@gmail.com>
2021-02-28 01:08:21 -0600

    added img

--- src/Controllers/AboutController.js
+++ src/Controllers/AboutController.js
@@ -5,7 +5,9 @@ class AboutController extends React.Component {
   render(){
     return(
       <div className="about-div" id="about">
-        <p>About</p>
+        <p>About
+        <img src="https://i.ibb.co/YRNL9VC/2-BD1472-C-B17-C-4-C09-B19-B-74-C56-C45-A138.jpg" alt="2-BD1472-C-B17-C-4-C09-B19-B-74-C56-C45-A138" border="0" />
+        </p>
       </div>
     )
   }
