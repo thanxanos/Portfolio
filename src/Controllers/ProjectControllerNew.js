3b6bb4d3495ac1e80acd42d70c4bcd86690231c8
zoexanos <zxanos@gmail.com>
2024-08-05 23:32:42 -0700

    minor changes to project hover

--- src/Controllers/ProjectControllerNew.js
+++ src/Controllers/ProjectControllerNew.js
@@ -13,81 +13,54 @@ class ProjectsControllerNew extends React.Component {
           <h1 data-aos="fade-up" delay="80" data-aos-duration="1050" className='portfoliotitle'>Portfolio</h1>
             <div data-aos="fade-up" delay="150" data-aos-duration="1200" className="projects-wrapper">
               <div className="project-card">
-
                 <div className="text-card">
                   <h2>Double Cryptography</h2>
-                  
                   <div className='button-div'><button className='lang-tag'>ruby on rails</button><button className='lang-tag'>react.js</button></div>
                   <p className='desc-text'>In-Browser React game. Play as a detective and solve puzzles.</p>
                     <img className='desc-image' src={doubleC} alt="doublC"></img>
-                    {/* <p className="mobile-text" >
-                      <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="mobile-link" /></a>
-                      <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="mobile-link" /></a>
-                      <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="mobile-link" /></a>
-                    </p> */}
+                      <p className="overlay" >
+                        <a title="video" href="https://youtu.be/-9TG-lpDn6c">VIDEO</a>
+                        <a title="git" href="https://github.com/zoexanos/Decoder-Frontend">GITHUB</a>
+                        <a title="live" href="https://double-cryptography.herokuapp.com/">LIVE</a>
+                      </p> 
                 </div>
               </div>
               <div className="project-card">
                 <div className="text-card">
                   <h2>Knight Quest</h2>
                   <div className='button-div'><button className='lang-tag'>ruby on rails</button></div>
-                  <p><img className='desc-image' src={knightQ} alt="knightQ"></img></p>
-                </div>
-                <div className="text-hover">
-                  <p className="p-text-hover">
-                  {/*<p>I created a browser-based game using Ruby on Rails for the back end and React for the front end. Users can log in to solve interactive puzzles with the help of a detective.</p>
-                  <p>For this project, I designed and wrote front end using React and React-konva. I implemented RESTful API to manage game state and persist game data. I also created all game graphics through Figma and used GSAP to manage animations
-                  </p>
-                  <div className="mobile-text" >
-                    <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="mobile-link" /></a>
-                    <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="mobile-link" /></a>
-                    <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="mobile-link" /></a>
-                  </div> */}
-                  <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="link-class" /></a>
-                  <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="link-class" /></a>
-                  <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="link-class" /></a></p>
+                  <p className='desc-text'>Terminal based RPG game. Play as a knight and forge your path.</p>
+                    <img className='desc-image' src={knightQ} alt="doublC"></img>
+                      <p className="overlay" >
+                        <a title="video" href="https://youtu.be/fnro7p4HKyM">VIDEO</a>
+                        <a title="git" href="https://github.com/zoexanos/Knight-Quest">GITHUB</a>
+                      </p> 
                 </div>
               </div>
               <div className="project-card">
                 <div className="text-card">
                   <h2>Double Cryptography</h2>
                   <div className='button-div'><button className='lang-tag'>ruby on rails</button><button className='lang-tag'>react.js</button></div>
-                  <p><img className='desc-image' src={doubleC} alt="doublC"></img></p>
-                </div>
-                <div className="text-hover">
-                  <p className="p-text-hover">
-                  {/*<p>I created a browser-based game using Ruby on Rails for the back end and React for the front end. Users can log in to solve interactive puzzles with the help of a detective.</p>
-                  <p>For this project, I designed and wrote front end using React and React-konva. I implemented RESTful API to manage game state and persist game data. I also created all game graphics through Figma and used GSAP to manage animations
-                  </p>
-                  <div className="mobile-text" >
-                    <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="mobile-link" /></a>
-                    <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="mobile-link" /></a>
-                    <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="mobile-link" /></a>
-                  </div> */}
-                  <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="link-class" /></a>
-                  <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="link-class" /></a>
-                  <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="link-class" /></a></p>
+                  <p className='desc-text'>In-Browser React game. Play as a detective and solve puzzles.</p>
+                    <img className='desc-image' src={doubleC} alt="doublC"></img>
+                      <p className="overlay" >
+                        <a title="video" href="https://youtu.be/-9TG-lpDn6c">VIDEO</a>
+                        <a title="git" href="https://github.com/zoexanos/Decoder-Frontend">GITHUB</a>
+                        <a title="live" href="https://double-cryptography.herokuapp.com/">LIVE</a>
+                      </p> 
                 </div>
               </div>
               <div className="project-card">
                 <div className="text-card">
                   <h2>Double Cryptography</h2>
                   <div className='button-div'><button className='lang-tag'>ruby on rails</button><button className='lang-tag'>react.js</button></div>
-                  <p><img className='desc-image' src={doubleC} alt="doublC"></img></p>
-                </div>
-                <div className="text-hover">
-                  <p className="p-text-hover">
-                  {/*<p>I created a browser-based game using Ruby on Rails for the back end and React for the front end. Users can log in to solve interactive puzzles with the help of a detective.</p>
-                  <p>For this project, I designed and wrote front end using React and React-konva. I implemented RESTful API to manage game state and persist game data. I also created all game graphics through Figma and used GSAP to manage animations
-                  </p>
-                  <div className="mobile-text" >
-                    <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="mobile-link" /></a>
-                    <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="mobile-link" /></a>
-                    <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="mobile-link" /></a>
-                  </div> */}
-                  <a title="video" href="https://youtu.be/-9TG-lpDn6c"><AiIcons.AiOutlinePlayCircle className="link-class" /></a>
-                  <a title="git" href="https://github.com/zoexanos/Decoder-Frontend"> <AiIcons.AiFillGithub className="link-class" /></a>
-                  <a title="live" href="https://double-cryptography.herokuapp.com/"> <DiIcons.DiHeroku className="link-class" /></a></p>
+                  <p className='desc-text'>In-Browser React game. Play as a detective and solve puzzles.</p>
+                    <img className='desc-image' src={doubleC} alt="doublC"></img>
+                      <p className="overlay" >
+                        <a title="video" href="https://youtu.be/-9TG-lpDn6c">VIDEO</a>
+                        <a title="git" href="https://github.com/zoexanos/Decoder-Frontend">GITHUB</a>
+                        <a title="live" href="https://double-cryptography.herokuapp.com/">LIVE</a>
+                      </p> 
                 </div>
               </div>
             </div>
